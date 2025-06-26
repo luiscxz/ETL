e1abb0ce76e777fd69a5a76b63a65b43a73ec3b4
Luiscxz <lionsgateazul@gmail.com>
2025-06-26 10:19:06 -0500

    #

--- ETL_postgreSQL.py
+++ ETL_postgreSQL.py
@@ -16,6 +16,32 @@ default_args = {
     'retry_delay':timedelta(minutes=2) # si falla reintenta en 2 minutos
 }
 
+
+#---------------------------------------------------------------------------#
+#                            Area de funciones                              #
+
+# función que se conecta a la API de mockaroo
+def _get_api():
+    import requests
+    url = 'https://my.api.mockaroo.com/a_pi.json'
+    headers = {'X-API-Key': '2d672d60'}
+    response = requests.get(url,headers)
+    with open('/tmp/sales_db_py.csv','wb') as file:
+        file.write(response.content)
+        file.close
+
+# función que transforma los datos
+def _join_trans():
+    import pandas as pd
+    df_py = pd.read_csv('/tmp/sales_db_py.csv')
+    df_bash = pd.read_csv('/tmp/sales_db_bash.csv')
+    df = pd.concat([df_py,df_bash], ignore_index=True)
+    df = df.groupby(['Fecha','Region'])['Costo'].sum().reset_index()
+    df.to_csv('/tmp/sales_db.csv',sep='\t', index=False, header=False)
+    print(df.head())
+
+
+
 # Procedemos a construir el DAG
 with DAG(
     'DAG_ETL_PostgreSQL',  # Nombre del DAG
@@ -26,3 +52,14 @@ with DAG(
     catchup=False,  # Para evitar ejecuciones retroactivas
     tags=['ETL', 'Ciencia de datos','PostgreSQL']
 ) as dag:
+    # area donde se llaman las funciones
+    get_api_python = PythonOperator(
+        task_id = 'get_api_python',
+        python_callable = _get_api
+    )
+
+    # definamos el bashOperator para que descargue la información
+    get_api_bash = BashOperator(
+        task_id = 'get_api_bash',
+        bash_command = 'curl -H "X-API-Key: 2d672d60" https://my.api.mockaroo.com/a_pi.json > /tmp/sales_db_bash.csv'
+    )
